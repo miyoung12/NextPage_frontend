9ce9a26ec158d6a04b34367d0f7bbc2931069196
miyoung12 <miseja3@gmail.com>
2024-06-07 18:10:15 +0900

    feat: intersection observer 적용

--- src/app/mypage/_component/BlockBoard.tsx
+++ src/app/mypage/_component/BlockBoard.tsx
@@ -10,8 +10,9 @@ interface BlockProps {
 
 export default function BlockBoard() {
   const [data, setData] = useState<BlockProps[]>([])
-  const [page, setPage] = useState(1) //현재 페이지 정보 저장
-  const [hasMore, setHasMore] = useState(true) //더 가져올 데이터가 있는지?
+  const [page, setPage] = useState(1) //스크롤이 닿았을 때 새롭게 데이터 페이지를 바꿀 state
+  const [hasMore, setHasMore] = useState(true) //더 가져올 데이터가 있는지? 데이터가 다 로드된 후 무한 스크롤이 멈추도록
+  const [isLoading, setIsLoading] = useState(false) // 데이터 로딩 중인지 여부를 나타내는 상태
   const loader = useRef<HTMLDivElement | null>(null) //무한 스크롤을 트리거하는 요소에 대한 참조
 
   const fetchData = (page: number) => {
@@ -39,9 +40,12 @@ export default function BlockBoard() {
         } else {
           setData((prevData) => [...prevData, ...newData.data])
         }
+        setIsLoading(false) // 데이터 로드가 끝난 후 로딩 상태 해제
       })
       .catch((error) => {
         console.error('Error:', error) // 에러 처리
+        setHasMore(false) // 에러 발생 시 hasMore를 false로 설정
+        setIsLoading(false) // 에러 발생 시 isLoading을 false로 설정
       })
   }
 
@@ -49,20 +53,23 @@ export default function BlockBoard() {
     //loader 요소가 뷰포트에 들어올 때마다 페이지 번호를 증가시킴
     //   fetchData(page)
     // }, [page])
-
-    if (hasMore) {
+    if (hasMore && !isLoading) {
       fetchData(page)
     }
-  }, [page])
+  }, [page, hasMore, isLoading])
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        if (entries[0].isIntersecting && hasMore) {
+        if (entries[0].isIntersecting && hasMore && !isLoading) {
           setPage((prevPage) => prevPage + 1)
+          // 데이터를 불러오고 화면에 렌더링 되기 전까지 계속 요건을 충족하므로
+          // page가 무한대로 증가하는 경우 방지
+          // 기존의 target을 unobserve함으로써 page가 한 번만 증가하게끔 함
+          //setHasMore(false) //**이게 있냐 없냐에 따라 무한히 스크롤이 되느냐 마느냐가 결정됨
+          console.log('hello Observer')
         }
-
-        console.log(entries)
+        console.log(entries) //유저의 화면에 들어왔거나 떠난 요소에 대한 정보
       },
       { threshold: 1.0 },
     )
@@ -76,10 +83,10 @@ export default function BlockBoard() {
         observer.unobserve(loader.current)
       }
     }
-  }, [hasMore])
+  }, [hasMore, isLoading])
 
   return (
-    <div className="flex flex-col justify-center items-center border w-[768px] h-[600px] overflow-auto border-white pb-[214px]">
+    <div className="flex flex-col items-center border rounded-[10px] w-[768px] h-[576px] overflow-auto border-white">
       <div className="grid grid-cols-4">
         {data.map((item, index) => (
           <Block key={index} content={item.content} imageUrl={item.imageUrl} />
@@ -90,7 +97,7 @@ export default function BlockBoard() {
           ))} */}
         <Plus />
       </div>
-      {hasMore && <div ref={loader} className="h-5" />}
+      {hasMore && !isLoading && <div ref={loader} className="" />}
     </div>
   )
 }
